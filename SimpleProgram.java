f73bce24f51d4e0116664341ad56153eb74862d5
veldhandikavya <veldhandikavya2002@gmail.com>
2026-06-23 13:18:09 +0530

    First Jenkins

---
+++ SimpleProgram.java
@@ -0,0 +1,6 @@
+public class SimpleProgram{
+public static void main(String[] args) {
+    System.out.print("Happy to code and learn the jenkins");
+    //System.out.println("Happy to code and learn the jenkins");
+}
+}
